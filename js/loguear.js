288e164e040cf4ba592b49628d44e45c77f2fb21
lucasuser22 <floresjeanluca@gmail.com>
2023-10-11 10:41:25 -0600

    fase1
    empezando la fase 1 del proyecto

---
+++ js/loguear.js
@@ -0,0 +1,28 @@
+function loguear() {
+    let user = document.getElementById("usuario");
+    let password = document.getElementById("clave");
+
+    if (user.value == "administrador" && password.value == "admini") {
+        window.location = "../gestion.html";
+        user.value = "";
+        password.value = "";
+        
+    }
+    else if(user.value == "cocinero" && password.value == "coock") {
+        window.location = "../cocinero.html";
+        user.value = "";
+    }
+    else if (user.value == "" || password.value == "") {
+        alert("Campos vacíos");
+    }
+    else {
+        alert("Datos incorrectos");
+    }
+
+    
+}
+
+function ocultarBoton() {
+    var boton = document.getElementById("boton-login");
+    boton.style.display = "none";
+  }
